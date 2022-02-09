e87b9e439f116b99a9e358d9a651b875480dddb4
Marcus Lira <marcuslira57@gmail.com>
2022-02-09 16:42:50 -0300

    Initial commit

---
+++ MongoDB/src/test/java/com/estudo/mongodb/MongoDB/MongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudo.mongodb.MongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
